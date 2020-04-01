5c0111c3e57ac9c576b5475d1b00f1490c3efbbe
Creeper Lv <creeperlv38@outlook.com>
2020-04-01 14:56:20 +0800

    Added configuration for linux(definition of Unity3D is missing.)

---
+++ CLUBS.Tools.Linux/ToolsInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CLUBS.Tools
+{
+    public class ToolsInfo
+    {
+        public const string CFG_PLATFORM = "Linux";
+        public readonly static Version ToolsInfoVer = new Version(1, 0, 0, 0);
+    }
+}
